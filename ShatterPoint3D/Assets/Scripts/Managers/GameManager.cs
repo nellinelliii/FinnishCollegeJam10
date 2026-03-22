7b4627aa73b3227cc7d91b97e2c6e323585ca391
nellinelliii <nelli.i.s.puttonen@gmail.com>
2026-03-22 10:09:03 +0200

    ok

---
+++ ShatterPoint3D/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Reference to the player GameObject
+    public GameObject PlayerObj;
+
+    // Singleton instance of the GameManager
+    public static GameManager Instance;
+
+
+    private void Awake()
+    {
+        if (Instance == null) //Tarkistetaan onko GameManager-instanssi olemassa
+        {
+            Debug.Log("GameManager created!", this.gameObject);
+            Instance = this; //Asetetaan t‰m‰ GameManager-instanssiksi
+            DontDestroyOnLoad(gameObject); // Estet‰‰n GameManagerin tuhoutuminen scene-latauksissa
+        }
+        else
+        {
+            Debug.LogError("GameManager exists already! Destroying duplicate."); // Virheilmoitus, jos toinen GameManager-instanssi on jo olemassa)
+            Destroy(gameObject); // Tuhoaa t‰m‰n GameManager-instanssin, jos toinen on jo olemassa
+        }
+    }
+
+}
---
+++ ShatterPoint3D/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject MainMenuPanel; // UI Paneeli Main Menua varten
+    public GameObject HUDpanel; // UI Paneeli HUD:ia varten
+
+    public static UIManager Instance;
+
+    private void Awake()
+    {
+
+
+        if (Instance == null)
+        {
+            Debug.Log("UIManager created!", this.gameObject);
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Debug.LogError("UIManager exists already! Destroying duplicate.");
+            Destroy(gameObject);
+        }
+    }
+
+    public void ShowMainMenuPanel()
+    {
+        MainMenuPanel.SetActive(true);
+        HUDpanel.SetActive(false);
+    }
+
+    public void ShowHUDpanel()
+    {
+        HUDpanel.SetActive(true);
+        MainMenuPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+   
+}
